714cf6e27f1bf24d58dd21928d31187f6dd66ab7
Jake O'Neill <joneill@hyperglance.com>
2023-05-11 11:45:17 +0100

    add metadata macros. use init_module correctly.

--- whereismypacket.c
+++ whereismypacket.c
@@ -1,16 +1,18 @@
-
 #include <linux/kernel.h>
 #include <linux/module.h>
-#include <linux/printk.h>
+#include <linux/init.h>
 
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Jake O'Neill");
+MODULE_DESCRIPTION("Determine why a packet was dropped");
+MODULE_VERSION("1.0");
 
-int init_module(void) {
+int __init startup(void) {
     printk("eep eep");
     return 0;
 }
 
-void cleanup_module(void) {
-
-}
+init_module(startup());
+#cleanup_module();
 
 
